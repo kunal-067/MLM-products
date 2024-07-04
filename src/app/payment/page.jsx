c04917495e164b5df8641e22ee31a9313d9b8858
itz-kunal <kunal.wd067@gmail.com>
2024-07-04 11:13:38 +0530

    upi change

--- src/app/payment/page.jsx
+++ src/app/payment/page.jsx
@@ -7,7 +7,7 @@ function Payment({searchParams}) {
             <div className="flex flex-col items-center justify-center h-screen">
             <h1 className="text-2xl font-bold mb-4">Payable Amount: ₹{searchParams.amount || 0}</h1>
                 <h1 className="text-2xl font-bold mb-4">Scan QR Code to Pay</h1>
-                <QRCode value={`upi://pay?pa=richtrek@axl`} size={256} /> <br /><br />
+                <QRCode value={`upi://pay?pa=9680974745@paytm`} size={256} /> <br /><br />
                 <b>OR, Pay on following UPI Id :</b>
                 <p className="mt-4">richtrek@axl</p>
             </div>
